fd6633af6762941a6db964d2afa2dc964011b946
Marian <muellerm187@gmail.com>
2025-12-31 20:12:57 +0100

    chore: add environment configuration (.env)

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rzxtjxuelcfkueqyqcft.supabase.co';
-const supabaseAnonKey = 'sb_publishable_SNN_EIrKiMe92RKBwJkVkQ_BSAL4T_L';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
