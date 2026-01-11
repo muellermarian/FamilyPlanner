4f55d90aa0dc524320ad9146e4246eced6e18a0c
Marian <muellerm187@gmail.com>
2026-01-11 16:41:36 +0100

    refactor: use replaceAll for better readability

--- src/hooks/usePushNotifications.ts
+++ src/hooks/usePushNotifications.ts
@@ -18,7 +18,7 @@ const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY || '';
 
 function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+  const base64 = (base64String + padding).replaceAll(/-/g, '+').replaceAll(/_/g, '/');
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
   for (let i = 0; i < rawData.length; ++i) {
